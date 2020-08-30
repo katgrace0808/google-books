685cbe6fe96fe7c411dcdb66687f2cb6e06ef9c8
Kathryn Teall <katgrace0808@gmail.com>
2020-08-30 13:23:06 -0700

    mLab setup

---
+++ client/build/precache-manifest.bf979e2f5565bb23de0e6ee462e48014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adda944b03053c05e8c8291fa0d051a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e67215b34bb763cb30a",
+    "url": "/static/css/main.6a7b5e56.chunk.css"
+  },
+  {
+    "revision": "cac8410aad3254cd62d5",
+    "url": "/static/js/2.e5ba9346.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.e5ba9346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e67215b34bb763cb30a",
+    "url": "/static/js/main.f8fea58b.chunk.js"
+  },
+  {
+    "revision": "bbac952799e545852822",
+    "url": "/static/js/runtime-main.084f65f5.js"
+  }
+]);
--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
 // Add routes, both API and view
 app.use(routes);
